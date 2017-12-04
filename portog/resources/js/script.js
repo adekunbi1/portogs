87316fc9e7bcdb08dc627c587a60428c584550d5
adekunbi1 <haptrendx@gmail.com>
2017-12-04 17:57:53 +0100

    Adding project source file

---
+++ portog/resources/js/script.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){ 
+   
+    
+//    sticky navigation snipper
+    $('.js--section-features').waypoint(function(direction){
+        if(direction == "down") {
+            $('nav').addClass('sticky');        
+        } else {
+            $('nav').removeClass('sticky');
+          }
+        }, {
+       offset: '60px'
+    
+        });  
+    
+
+//    scroll up button
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    
+//    Navigation scroll
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length){
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }        
+        });  
+    });
+    
+    
+//    Animation on scroll
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+        $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+        $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+        $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+   
+//    navigation grid
+    $('.js--nav-icon').click(function(){
+       var nav = $('.js--main-nav');
+       var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200); //0.2 miliseconds
+        if(icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }    
+    });
+    
+    
+    /*gmaps snippet*/
+    
+    var map = new GMaps({
+      div: '.map',
+//      lat: -12.043333,
+      lat: 6.6018,
+      lng: 3.3515,
+//      lng: -77.028333,
+       zoom: 12
+    });
+    
+    
+    map.addMarker({
+        lat: -12.043333,
+        lng: -77.028333,
+        title: 'Lima',
+        infoWindow: {
+        content: '<p>Lima HQ</p>'
+        }
+    });
+    
+    
+});
